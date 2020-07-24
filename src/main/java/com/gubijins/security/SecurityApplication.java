6554cacd2fa489f9c3c33e7185a7c52b26bbbd38
Gubijins <18717962102.com>
2020-07-24 14:00:38 +0800

    chapter7 dept_service

--- src/main/java/com/gubijins/security/SecurityApplication.java
+++ src/main/java/com/gubijins/security/SecurityApplication.java
@@ -3,6 +3,8 @@ package com.gubijins.security;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.gubijins.security.mapper")
@@ -12,4 +14,11 @@ public class SecurityApplication {
         SpringApplication.run(SecurityApplication.class, args);
     }
 
+    @Bean
+    public InternalResourceViewResolver setupViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 }
--- src/main/java/com/gubijins/security/config/WebMvcConfiguration.java
+++ src/main/java/com/gubijins/security/config/WebMvcConfiguration.java
@@ -14,21 +14,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 
 
-@Configuration
-//public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
-public class WebMvcConfiguration extends WebMvcConfigurationSupport {
-
-        @Autowired
-        HttpInterceptor httpInterceptor;
-
-        @Override
-        public void addInterceptors(InterceptorRegistry registry) {
-            // addPathPatterns 用于添加拦截规则
-            // excludePathPatterns 用户排除拦截
-            // 映射为 user 的控制器下的所有映射
-            //registry.addInterceptor(httpInterceptor).addPathPatterns("/admin/*").excludePathPatterns("/index", "/");
-            registry.addInterceptor(httpInterceptor);
-            super.addInterceptors(registry);
-        }
-
-}
+//@Configuration
+////public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+//public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+//
+//        @Autowired
+//        HttpInterceptor httpInterceptor;
+//
+//        @Override
+//        public void addInterceptors(InterceptorRegistry registry) {
+//            // addPathPatterns 用于添加拦截规则
+//            // excludePathPatterns 用户排除拦截
+//            // 映射为 user 的控制器下的所有映射
+//            //registry.addInterceptor(httpInterceptor).addPathPatterns("/admin/*").excludePathPatterns("/index", "/");
+//            //registry.addInterceptor(httpInterceptor);
+//            super.addInterceptors(registry);
+//        }
+//
+//}
